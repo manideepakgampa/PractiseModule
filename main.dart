4f734b540d21ed4e5df57f1a50802e3294b7bd1a
“deepu” <manideepakgampa21@gmail.com>
2024-12-10 15:31:11 +0530

    Second Commit

--- main.dart
+++ main.dart
@@ -1,3 +1,12 @@
+/*
 main() {
   print("hello");
+}
+*/
+import 'dart:io';
+
+void main() {
+  stdout.writeln("Enter you name: ");
+  var name = stdin.readLineSync();
+  print('My name is $name');
 }
