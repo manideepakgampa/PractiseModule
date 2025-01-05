aef97186fb7b6f48b81611806457211a714bdffb
“deepu” <manideepakgampa21@gmail.com>
2025-01-05 21:36:41 +0530

    TypeConvo Commit

--- main.dart
+++ main.dart
@@ -6,11 +6,24 @@ main() {
 import 'dart:io';
 
 void main() {
-  stdout.writeln("Enter your name: ");
-  var name = stdin.readLineSync() ?? 'Unknown';
+  // stdout.writeln("Enter your name: ");
+  // var name = stdin.readLineSync() ?? 'Unknown';
 
-  stdout.writeln("Enter the number: ");
-  int i = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+  // stdout.writeln("Enter the number: ");
+  // int i = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
 
-  print('My name is $name and The Number is $i');
+  // print('My name is $name and The Number is $i');
+  // var x = '1';
+  // var y = '1.1';
+
+  // var one = int.parse(x);
+  // assert(one == 1);
+  // var onepone = double.parse(y);
+  // assert(onepone == 1.1);
+
+  // stdout.writeln(x.runtimeType);
+  // stdout.writeln(one.runtimeType);
+
+  // stdout.writeln(y.runtimeType);
+  // stdout.writeln(onepone.runtimeType);
 }
